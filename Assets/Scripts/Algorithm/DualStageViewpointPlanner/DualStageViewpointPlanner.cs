8f249f6385b318232d8e51730c48371b3f774a87
Kasperjhn <kjhn@live.dk>
2023-03-31 01:09:35 +0200

    Update DualStageViewpointPlanner.cs

--- Assets/Scripts/Algorithm/DualStageViewpointPlanner/DualStageViewpointPlanner.cs
+++ Assets/Scripts/Algorithm/DualStageViewpointPlanner/DualStageViewpointPlanner.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using MAES3D.Agent;
 using MAES3D.Algorithm;
 using UnityEngine;
+using System.Linq;
 
 namespace MAES3D.Algorithm.DualStageViewpointPlanner {
     public class DualStageViewpointPlanner : IAlgorithm
@@ -13,6 +14,8 @@ namespace MAES3D.Algorithm.DualStageViewpointPlanner {
         public List<Cell> localFrontiers = new List<Cell>();
         
         public List<Cell> globalFrontiers = new List<Cell>();
+
+        public Vector3 lastExplorationDirection = new Vector3(0,0,1);
 
         private RRT RRTmap;
 
@@ -66,35 +69,116 @@ namespace MAES3D.Algorithm.DualStageViewpointPlanner {
 
             if(!done){
                 RRTmap = new RRT(_controller.GetPosition());
-                BuildRRT();
-                FrontierFinder9001();
+                BuildRRT(FindFrontiers(5));
                 done = true;
             }
             Debug.Break();
         }
 
         public void ExploreStage(){
-
+            
+            //Set S_lb, root position P_rob and F_local
+            //Update F_LS
+            //V <- DynamicRRT()
+            BuildRRT(FindFrontiers(5));
+            //BestGain <- 0
+            //FindBestGainViewpoint();
+            //for i from 1 to N do
+            //    Compute Gain(Bi)
+            //    if Gain(Bi) > BestGain then
+            //        BestGain ← Gain(Bi)
+            //        BestBranch ← Bi
+            //    end
+            //end
+            //Previous Tree ← Current Tree
+            
         }
 
         public void DynamicExpansion(){
-
+            /*
+            Set Slb, root position Prob and FS
+            Prune(Previous Tree)
+            Rebuild(Previous Tree)
+            while Nnew < N do
+                Sample u ∼ U[0, 1]
+                if u <= θ then
+                    Random Sample viewpoints in Slb
+                else
+                    Random Sample viewpoints around FS
+                end
+            end
+            */
         }
 
         public void RelocationStage(){
-
+            /*
+            Update Fglobal and G
+            Flag ← F alse and Dist ← 0
+            for i from N to 1 do
+                for j from M to 1 do
+                    if Fi in FOV(vi) then
+                        vS ← vi, FGS ← Fi
+                        Dist ← Dist(Fi, vi), Flag ← T rue
+                        break;
+                    end
+                    if Flag is True then
+                        break;
+                    end
+                end
+            end
+            if Flag is True then
+                for i from N to 1 do
+                    if FGS in FOV(vi) then
+                        if Dist(FGS, vi) < Dist then
+                            vS ← vi
+                        end
+                    end
+                end
+            else
+                Exploration Complete.
+            end
+            */
         }
 
-        public void BuildRRT()
-        {
-            for (int i = 0; i < RRTmap.maxIterations; i++)
+        public RRTnode FindBestGainViewpoint(RRT tree){
+            RRTnode bestCell = new RRTnode(new Vector3(0,0,0));
+            
+            return calculateGain(tree.root, bestCell);
+        }
+
+        public RRTnode calculateGain(RRTnode treeNode, RRTnode currBestNode, float prevGain = 0, float currBestGain = 0, int branchLength = 0){
+            foreach (RRTnode child in treeNode.children)
             {
-                Vector3 randomPoint = RRTmap.root.position + RRTmap.RandomPoint();
-
-                AddPointToClosestNode(RRTmap.root, randomPoint);
+                float gain = 0;
+                if (gain + prevGain > currBestGain)
+                    currBestGain = gain + prevGain;
+                    currBestNode = child;
+                calculateGain(child, currBestNode, gain + prevGain, currBestGain, branchLength+1);                
             }
-            RRTmap.TraversePreOrder(RRTmap.root);
+            return currBestNode;
         }
+
+        public void BuildRRT(List<Cell> frontiers, int iterations = 500)
+        {
+            float threshold = 0.30f; //x% chance to select a point next to frontier
+            for (int i = 0; i < iterations; i++)
+            {
+                Vector3 targetPoint; 
+                if (Random.Range(0f,1f) < threshold)
+                {
+                    Cell selectedFrontier = frontiers[Random.Range(0,frontiers.Count)];
+                    targetPoint = selectedFrontier.middle + new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), Random.Range(-1f, 1f));
+                }
+                else
+                {                    
+                    targetPoint = RRTmap.root.position + new Vector3(Random.Range(-10f, 10f), Random.Range(-10f, 10f), Random.Range(-10f, 10f));
+                }
+                AddPointToClosestNode(RRTmap.root, targetPoint);
+
+            }
+            RRTmap.DrawTree(RRTmap.root);
+        }
+        
         public void AddPointToClosestNode(RRTnode root, Vector3 p){
             // Find the closest node in the tree to p
             RRTnode closestNode = RRTmap.FindNearestNode(root, p);
@@ -106,7 +190,7 @@ namespace MAES3D.Algorithm.DualStageViewpointPlanner {
             }
         }
 
-        public void FrontierFinder9001(){
+        public List<Cell> FindFrontiers(int frontierAmount){
             List<Cell> viewableCells = _controller.GetVisibleCells();
             foreach (Cell cell in viewableCells)
             {
@@ -119,15 +203,54 @@ namespace MAES3D.Algorithm.DualStageViewpointPlanner {
                         _controller.GetExplorationStatusOfCell(new Cell(cell.x, cell.y, cell.z + 1), true) == CellStatus.unexplored ||
                         _controller.GetExplorationStatusOfCell(new Cell(cell.x, cell.y, cell.z - 1), true) == CellStatus.unexplored)
                     {
-                        localFrontiers.Add(cell);
+                        bool clusterCheck = false; // Checks if another frontier is nearby
+                        foreach (Cell frontier in localFrontiers)  
+                        {
+                            if ((cell.toVector - frontier.toVector).magnitude < 5) // Checks if another frontier is nearby
+                                clusterCheck = true;
+                                continue;
+                        }
+                        if(!clusterCheck)
+                            localFrontiers.Add(cell);
                     }
                 }
-            }
-            foreach (Cell c in localFrontiers)
+            } 
+            foreach (Cell c in FindBestFrontiers(localFrontiers, frontierAmount)) //FindBestFrontiers(localFrontiers, frontierAmount) || localFrontiers
             {
-                Debug.DrawLine(new Vector3(c.x-0.01f, c.y-0.01f, c.z-0.01f), c.middle, Color.red, 5000);
+                c.DrawCell();
             }
-        }        
+            return FindBestFrontiers(localFrontiers, frontierAmount);
+        }
+
+        public List<Cell> FindBestFrontiers(List<Cell> frontiers, int amount){ //find x best cells in the drone's direction
+            Dictionary<Cell, float> bestFrontiers = new Dictionary<Cell, float>();
+            
+            foreach (Cell frontier in frontiers)
+            {
+                Vector3 frontierDirection = (frontier.middle - _controller.GetPosition()).normalized;
+
+                float similarity = Vector3.Angle(frontierDirection, lastExplorationDirection.normalized);
+                similarity = (lastExplorationDirection - frontierDirection).magnitude;
+                if (bestFrontiers.Count < amount)
+                {
+                    bestFrontiers.Add(frontier, similarity);
+                }
+                else
+                {
+                    float maxSimilarity = bestFrontiers.Values.Max();
+                    if ( maxSimilarity > similarity)
+                    {
+                        var largestKey = bestFrontiers.FirstOrDefault(x => x.Value == maxSimilarity).Key;
+                        bestFrontiers.Remove(largestKey);
+                        bestFrontiers.Add(frontier, similarity);
+                    }
+
+                }
+            }
+            return new List<Cell>(bestFrontiers.Keys);
+        }      
+        
+          
     }
 
     public class RRTnode{
@@ -144,7 +267,6 @@ namespace MAES3D.Algorithm.DualStageViewpointPlanner {
 
     public class RRT
     {
-        public int maxIterations = 500;
         public float stepSize = 1f;
         public float goalRadius = 0.5f;
 
@@ -155,14 +277,7 @@ namespace MAES3D.Algorithm.DualStageViewpointPlanner {
 
         }
 
-        public Vector3 RandomPoint()
-        {
-            float x = Random.Range(-10f, 10f);
-            float y = Random.Range(-10f, 10f);
-            float z = Random.Range(-10f, 10f);
 
-            return new Vector3(x, y, z);
-        }
 
         public Vector3 Steer(RRTnode node, Vector3 target)
         {
@@ -194,7 +309,7 @@ namespace MAES3D.Algorithm.DualStageViewpointPlanner {
             }
             return closestNode;
         }
-        public void TraversePreOrder(RRTnode node, bool alternate = true){
+        public void DrawTree(RRTnode node, bool alternate = true){
             if(node == null) return;
             // Visit the current node
             foreach(RRTnode child in node.children){
@@ -202,7 +317,7 @@ namespace MAES3D.Algorithm.DualStageViewpointPlanner {
                     Debug.DrawLine(node.position, child.position, Color.white, 5000);
                 else
                     Debug.DrawLine(node.position, child.position, Color.black, 5000);
-                TraversePreOrder(child, !alternate);
+                DrawTree(child, !alternate);
             }
             // Traverse the right subtree
             // Not applicable since this is a tree, not a binary tree
