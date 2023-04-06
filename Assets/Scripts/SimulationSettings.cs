dbee2c6a5ca1c0546bee6360e01330a632c07c00
Kasperjhn <kjhn@live.dk>
2023-04-06 09:34:30 +0200

    Dynamic Expansion

--- Assets/Scripts/SimulationSettings.cs
+++ Assets/Scripts/SimulationSettings.cs
@@ -14,7 +14,7 @@ public static class SimulationSettings
     public static int agentCount = 1;
     public static int algorithm = 1; // 0 = RBW, 1 = LVD
 
-    public static float duration = 30*60; // in minutes
+    public static float duration = 30 * 60; // in minutes
 
     public static float progress = 0;
 }
