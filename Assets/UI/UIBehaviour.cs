08e0b33ea502f1c37501b0555ba4d026dd696074
Kasperjhn <kjhn@live.dk>
2023-04-19 11:58:36 +0200

    Update UIBehaviour.cs
    A lot of restructuring and bugfixes

--- Assets/UI/UIBehaviour.cs
+++ Assets/UI/UIBehaviour.cs
@@ -18,140 +18,53 @@ public class UIBehaviour : MonoBehaviour
 
     public UIDocument CaveUI;
     public UIDocument AgentUI;
-    public UIDocument CameraControls;
+    public UIDocument CameraUI;
 
     private List<SubmarineAgent> agents;
     private int agentIndex;
 
+    //CaveUI Interactables
+    private DropdownField dropdownAlgorithm;
+    private    SliderInt agentCount;
+    private    SliderInt duration;
+    private    SliderInt mapHeight;
+    private    SliderInt mapWidth;
+    private    SliderInt mapDepth;
+    private    Toggle toggleSeed;
+    private    TextField txtSeed;
+    private    ProgressBar progBar;
+    private    Button btnStart;
+    private    Button btnStop;
+    //CameraUI Interactable
+    private    Button btnPrevCam;
+    private    Button btnCave;
+    private    Button btnNextCam;
+
 
     private void OnEnable(){
+        /*
+        dropdownAlgorithm = CaveRoot.Q<DropdownField>("DropdownAlgorithm");
+        agentCount = CaveRoot.Q<SliderInt>("AgentCount");
+        duration = CaveRoot.Q<SliderInt>("SimDuration");
+        mapHeight = CaveRoot.Q<SliderInt>("MapHeight");
+        mapWidth = CaveRoot.Q<SliderInt>("MapWidth");
+        mapDepth = CaveRoot.Q<SliderInt>("MapDepth");
+        toggleSeed = CaveRoot.Q<Toggle>("ToggleRandomSeed");
+        txtSeed = CaveRoot.Q<TextField>("TxtSeed");
+        progBar = CaveRoot.Q<ProgressBar>("ProgressBar");
+        btnStart = CaveRoot.Q<Button>("ButtonStart");
+        btnStop = CaveRoot.Q<Button>("ButtonStop");
 
-        VisualElement CaveRoot = CaveUI.rootVisualElement;
+        btnPrevCam = CameraUI.rootVisualElement.Q<Button>("ButtonPrev");
+        btnCave = CameraUI.rootVisualElement.Q<Button>("ButtonCave");
+        btnNextCam = CameraUI.rootVisualElement.Q<Button>("ButtonNext");
+        */
 
-        DropdownField dropdownAlgorithm = CaveRoot.Q<DropdownField>("DropdownAlgorithm");
-
-        SliderInt agentCount = CaveRoot.Q<SliderInt>("AgentCount");
-        SliderInt duration = CaveRoot.Q<SliderInt>("SimDuration");
-        SliderInt mapHeight = CaveRoot.Q<SliderInt>("MapHeight");
-        SliderInt mapWidth = CaveRoot.Q<SliderInt>("MapWidth");
-        SliderInt mapDepth = CaveRoot.Q<SliderInt>("MapDepth");
-
-        Toggle toggleSeed = CaveRoot.Q<Toggle>("ToggleRandomSeed");
-        TextField txtSeed = CaveRoot.Q<TextField>("TxtSeed");
-
-        ProgressBar progBar = CaveRoot.Q<ProgressBar>("ProgressBar");
-
-        Button btnStart = CaveRoot.Q<Button>("ButtonStart");
-        Button btnStop = CaveRoot.Q<Button>("ButtonStop");
-
-        Button btnPrevCam = CameraControls.rootVisualElement.Q<Button>("ButtonPrev");
-        Button btnCave = CameraControls.rootVisualElement.Q<Button>("ButtonCave");
-        Button btnNextCam = CameraControls.rootVisualElement.Q<Button>("ButtonNext");
-
-
-        // Fixing TextFieldBugs
-        /**/
-        List<VisualElement> textFields = new UQueryBuilder<VisualElement>(CaveRoot).Name("unity-text-field").ToList();
-        foreach (TextField tf in textFields){
-            tf.label = tf.value;
-        }
-
-        agentCount.RegisterValueChangedCallback(v =>{
-            agentCount.Q<TextField>("unity-text-field").label = agentCount.value.ToString();
-        });
-        duration.RegisterValueChangedCallback(v =>{
-            duration.Q<TextField>("unity-text-field").label = duration.value.ToString();
-        });
-        mapHeight.RegisterValueChangedCallback(v =>{
-            mapHeight.Q<TextField>("unity-text-field").label = mapHeight.value.ToString();
-        });
-        mapWidth.RegisterValueChangedCallback(v =>{
-            mapWidth.Q<TextField>("unity-text-field").label = mapWidth.value.ToString();
-        });
-        mapDepth.RegisterValueChangedCallback(v =>{
-            mapDepth.Q<TextField>("unity-text-field").label = mapDepth.value.ToString();
-        });
-
-        toggleSeed.RegisterValueChangedCallback(v =>{
-            if (txtSeed.focusable = !toggleSeed.value)
-                txtSeed.value = "Insert Seed";
-            else
-                txtSeed.value = "Random Seed";
-        });
-
-        btnStart.clickable.clicked += () => {
-            if ((Simulation)FindObjectOfType(typeof(Simulation)) != null)
-            {
-                Debug.Log("Tried to start a simulation while another simulation is running.");
-                return;
-            }
-            CameraControls.enabled = true; // Enables the camera controls UI
-
-            // Adds values from VisualElements to SimulationSettings
-            SimulationSettings.algorithm = dropdownAlgorithm.index;
-            SimulationSettings.agentCount = agentCount.value;
-            SimulationSettings.duration = duration.value*60; // in minutes
-            SimulationSettings.useRandomSeed = toggleSeed.value;
-            if (!SimulationSettings.useRandomSeed)
-            {
-                SimulationSettings.seed = Int32.Parse(txtSeed.value);
-            }
-            SimulationSettings.Height = mapHeight.value;
-            SimulationSettings.Width = mapWidth.value;
-            SimulationSettings.Depth = mapDepth.value;
-            
-            // Starts the simulation
-            Sim.SetupSimulation(SimulationSettings.duration);
-            timeLeft = SimulationSettings.duration;
-
-            // Start Taking Results
-            TextWriter textWriter = new StreamWriter(Application.dataPath + $"/Results/{AlgorithmIndexToString(SimulationSettings.algorithm)}_x{SimulationSettings.Width}y{SimulationSettings.Height}z{SimulationSettings.Depth}_I{SimulationSettings.Instance}.csv", false);
-            textWriter.WriteLine($"Algorithm,{AlgorithmIndexToString(SimulationSettings.algorithm)}");
-            textWriter.WriteLine($"MapSize,x={SimulationSettings.Width},y={SimulationSettings.Height},z={SimulationSettings.Depth}");
-            textWriter.WriteLine($"AgentAmount,{SimulationSettings.agentCount}");
-            textWriter.WriteLine($"Duration(min),{SimulationSettings.duration/60}");
-            textWriter.WriteLine($"Seed,{SimulationSettings.seed}");
-            textWriter.WriteLine("Timestamp(s),Progress(%),");
-            if (SimulationSettings.Instance == 0)
-                textWriter.WriteLine("0, 0");
-            textWriter.Close();
-
-            mainCamera.transform.position = new Vector3(-(mapWidth.value * 0.25f), mapHeight.value * 1.25f, -(mapDepth.value * 0.25f));
-            mainCamera.transform.LookAt(new Vector3(mapWidth.value * 0.5f, mapHeight.value * 0.5f, mapDepth.value * 0.5f));
-            mainCamera.transform.Translate(Vector3.right * (Mathf.Sqrt(mapWidth.value ^ 2 * mapDepth.value ^ 2) * 0.2f), Space.Self);
-
-            agents = new List<SubmarineAgent>(FindObjectsOfType<SubmarineAgent>());
-            agentIndex = 0;
-
-        };
-        btnStop.clickable.clicked += () => {
-            CameraControls.enabled = false; // Disables the camera controls UI
-
-            timeLeft = 0;
-            Sim.DestroySimulation();
-
-        };
-
-        btnPrevCam.clickable.clicked += () => 
-        {
-            Debug.Log("Prev Clicked");
-
-            ChangeCam(-1);
-        };
-
-        btnCave.clickable.clicked += () => 
-        {
-            Debug.Log("Cave Clicked");
-
-            ChangeCam(0);
-        };
-
-        btnNextCam.clickable.clicked += () => 
-        {
-            Debug.Log("Next Clicked");
-
-            ChangeCam(1);
-        };
+        UpdateButtons(CaveUI);
+        /*
+        UpdateButtons(CameraUI);
+        UpdateButtons(AgentUI);
+        */
     }
 
     void FixedUpdate(){
@@ -192,7 +105,7 @@ public class UIBehaviour : MonoBehaviour
     }
 
 
-    private void ChangeCam(int movement){
+    private void ChangeCam(){
 
         if (mainCamera != null)
         {
@@ -200,53 +113,27 @@ public class UIBehaviour : MonoBehaviour
             CameraController cameraController = mainCamera.GetComponent<CameraController>();
             GameObject chunk = GameObject.Find("Chunk(Clone)");
 
-
-            switch (movement)
+            if (agentIndex == 0)
             {
-                case 0:
-                    ChangeToUI(CaveUI);
-                    cameraController.SetTargetOffset(chunk.transform, new Vector3(SimulationSettings.Width / 2, SimulationSettings.Height / 2, SimulationSettings.Depth / 2));
-                    agentIndex = 0;
-                    return;
-
-                case 1:
-                    agentIndex++;
-                    if (agentIndex > agents.Count)
-                    {
-                        agentIndex = 0;
-                        cameraController.SetTargetOffset(chunk.transform, new Vector3(SimulationSettings.Width / 2, SimulationSettings.Height / 2, SimulationSettings.Depth / 2));
-                        ChangeToUI(CaveUI);
-                    }
-                    else
-                    {
-                        cameraController.SetTarget(agents[agentIndex-1].transform);
-                        ChangeToUI(AgentUI);
-                    }
-                    return;
-
-                case -1:
-                    agentIndex--;
-                    if (agentIndex == 0)
-                    {
-                        cameraController.SetTargetOffset(chunk.transform, new Vector3(SimulationSettings.Width / 2, SimulationSettings.Height / 2, SimulationSettings.Depth / 2));
-                        ChangeToUI(CaveUI);
-                    }
-                    else if (agentIndex > 0)
-                    {
-                        cameraController.SetTarget(agents[agentIndex-1].transform);
-                        ChangeToUI(AgentUI);
-                    }
-                    else
-                    {
-                        agentIndex = agents.Count;
-                        cameraController.SetTarget(agents[agentIndex-1].transform);
-                        ChangeToUI(AgentUI);
-                    }
-                    return;
-                default:
-                    Debug.LogError("Invalid CameraMovement");
-                    Debug.Break();
-                    return;
+                ChangeToUI(CaveUI);
+                cameraController.SetTargetOffset(chunk.transform, new Vector3(SimulationSettings.Width / 2, SimulationSettings.Height / 2, SimulationSettings.Depth / 2));
+            }
+            else if (agentIndex > agents.Count)
+            {
+                ChangeToUI(CaveUI);
+                cameraController.SetTargetOffset(chunk.transform, new Vector3(SimulationSettings.Width / 2, SimulationSettings.Height / 2, SimulationSettings.Depth / 2));
+                agentIndex = 0;
+            }
+            else if (agentIndex < 0)
+            {
+                agentIndex = agents.Count;
+                cameraController.SetTarget(agents[agentIndex-1].transform);
+                ChangeToUI(AgentUI);
+            }
+            else
+            {
+                cameraController.SetTarget(agents[agentIndex-1].transform);
+                ChangeToUI(AgentUI);
             }
         }
     }
@@ -256,11 +143,153 @@ public class UIBehaviour : MonoBehaviour
         {
             CaveUI.enabled = false;
             AgentUI.enabled = true;
+            UpdateButtons(AgentUI);
         }
         else if (newUI == CaveUI)
         {
             AgentUI.enabled = false;
             CaveUI.enabled = true;
+            UpdateButtons(CaveUI);
+        }
+        else
+        {
+            Debug.LogError("Not Valid UI Selected");
+            Debug.Break();
+        }
+    }
+
+    private void UpdateButtons(UIDocument UIDoc){
+        if (UIDoc == CameraUI)
+        {
+            btnPrevCam = CameraUI.rootVisualElement.Q<Button>("ButtonPrev");
+            btnCave = CameraUI.rootVisualElement.Q<Button>("ButtonCave");
+            btnNextCam = CameraUI.rootVisualElement.Q<Button>("ButtonNext");
+            
+            btnPrevCam.clickable.clicked += () => 
+            {
+                Debug.Log("Prev Clicked");
+                agentIndex--;
+                ChangeCam();
+            };
+
+            btnCave.clickable.clicked += () => 
+            {
+                Debug.Log("Cave Clicked");
+                agentIndex = 0;
+                ChangeCam();
+            };
+
+            btnNextCam.clickable.clicked += () => 
+            {
+                Debug.Log("Next Clicked");
+                agentIndex++;
+                ChangeCam();
+            };
+        }
+        else if (UIDoc == CaveUI)
+        {
+            Debug.Log("CaveUI Updated");
+            VisualElement CaveRoot = CaveUI.rootVisualElement;
+
+            //Update References to Interactables
+            dropdownAlgorithm = CaveRoot.Q<DropdownField>("DropdownAlgorithm");
+            agentCount = CaveRoot.Q<SliderInt>("AgentCount");
+            duration = CaveRoot.Q<SliderInt>("SimDuration");
+            mapHeight = CaveRoot.Q<SliderInt>("MapHeight");
+            mapWidth = CaveRoot.Q<SliderInt>("MapWidth");
+            mapDepth = CaveRoot.Q<SliderInt>("MapDepth");
+            toggleSeed = CaveRoot.Q<Toggle>("ToggleRandomSeed");
+            txtSeed = CaveRoot.Q<TextField>("TxtSeed");
+            progBar = CaveRoot.Q<ProgressBar>("ProgressBar");
+            btnStart = CaveRoot.Q<Button>("ButtonStart");
+            btnStop = CaveRoot.Q<Button>("ButtonStop");
+
+            // Fixing TextFieldBugs
+            FixVisualBug();
+
+            //Update Interactables when interacted with
+            agentCount.RegisterValueChangedCallback(v =>{
+                agentCount.Q<TextField>("unity-text-field").label = agentCount.value.ToString();
+            });
+            duration.RegisterValueChangedCallback(v =>{
+                duration.Q<TextField>("unity-text-field").label = duration.value.ToString();
+            });
+            mapHeight.RegisterValueChangedCallback(v =>{
+                mapHeight.Q<TextField>("unity-text-field").label = mapHeight.value.ToString();
+            });
+            mapWidth.RegisterValueChangedCallback(v =>{
+                mapWidth.Q<TextField>("unity-text-field").label = mapWidth.value.ToString();
+            });
+            mapDepth.RegisterValueChangedCallback(v =>{
+                mapDepth.Q<TextField>("unity-text-field").label = mapDepth.value.ToString();
+            });
+            toggleSeed.RegisterValueChangedCallback(v =>{
+                if (txtSeed.focusable = !toggleSeed.value)
+                    txtSeed.value = "Insert Seed";
+                else
+                    txtSeed.value = "Random Seed";
+            });
+
+            btnStart.clickable.clicked += () => {
+                Debug.Log("Start Clicked");
+
+                if ((Simulation)FindObjectOfType(typeof(Simulation)) != null)
+                {
+                    Debug.Log("Tried to start a simulation while another simulation is running.");
+                    return;
+                }
+                CameraUI.enabled = true; // Enables the camera controls UI
+                UpdateButtons(CameraUI);
+
+
+                // Adds values from VisualElements to SimulationSettings
+                SimulationSettings.algorithm = dropdownAlgorithm.index;
+                SimulationSettings.agentCount = agentCount.value;
+                SimulationSettings.duration = duration.value*60; // in minutes
+                SimulationSettings.useRandomSeed = toggleSeed.value;
+                if (!SimulationSettings.useRandomSeed)
+                {
+                    SimulationSettings.seed = Int32.Parse(txtSeed.value);
+                }
+                SimulationSettings.Height = mapHeight.value;
+                SimulationSettings.Width = mapWidth.value;
+                SimulationSettings.Depth = mapDepth.value;
+                
+                // Starts the simulation
+                Sim.SetupSimulation(SimulationSettings.duration);
+                timeLeft = SimulationSettings.duration;
+
+                // Start Taking Results
+                TextWriter textWriter = new StreamWriter(Application.dataPath + $"/Results/{AlgorithmIndexToString(SimulationSettings.algorithm)}_x{SimulationSettings.Width}y{SimulationSettings.Height}z{SimulationSettings.Depth}_I{SimulationSettings.Instance}.csv", false);
+                textWriter.WriteLine($"Algorithm,{AlgorithmIndexToString(SimulationSettings.algorithm)}");
+                textWriter.WriteLine($"MapSize,x={SimulationSettings.Width},y={SimulationSettings.Height},z={SimulationSettings.Depth}");
+                textWriter.WriteLine($"AgentAmount,{SimulationSettings.agentCount}");
+                textWriter.WriteLine($"Duration(min),{SimulationSettings.duration/60}");
+                textWriter.WriteLine($"Seed,{SimulationSettings.seed}");
+                textWriter.WriteLine("Timestamp(s),Progress(%),");
+                if (SimulationSettings.Instance == 0)
+                    textWriter.WriteLine("0, 0");
+                textWriter.Close();
+
+                mainCamera.transform.position = new Vector3(-(mapWidth.value * 0.25f), mapHeight.value * 1.25f, -(mapDepth.value * 0.25f));
+                mainCamera.transform.LookAt(new Vector3(mapWidth.value * 0.5f, mapHeight.value * 0.5f, mapDepth.value * 0.5f));
+                mainCamera.transform.Translate(Vector3.right * (Mathf.Sqrt(mapWidth.value ^ 2 * mapDepth.value ^ 2) * 0.2f), Space.Self);
+
+                agents = new List<SubmarineAgent>(FindObjectsOfType<SubmarineAgent>());
+                agentIndex = 0;
+            };
+            btnStop.clickable.clicked += () => {
+                Debug.Log("Stop Clicked");
+                CameraUI.enabled = false; // Disables the camera controls UI
+
+                timeLeft = 0;
+                Sim.DestroySimulation();
+
+            };
+        }
+        else if (UIDoc == AgentUI)
+        {
+            
         }
         else
         {
@@ -275,9 +304,25 @@ public class UIBehaviour : MonoBehaviour
         tw.Close();
     }
 
-    private void UpdateAgentUI(int agentID, String task, Vector3 position, Vector3 target, string algoInfo = null){
-        
+    private void UpdateAgentUI(int agentID, String task, Vector3 position, Vector3 target, string algoInfo = null)
+    {
 
+    }
+
+    private void FixVisualBug(){
+        List<VisualElement> textFields = new UQueryBuilder<VisualElement>(CaveUI.rootVisualElement).Name("unity-text-field").ToList();
+        foreach (TextField tf in textFields){
+            tf.label = tf.value;
+        }
+
+        dropdownAlgorithm.index = SimulationSettings.algorithm;
+
+        agentCount.value = SimulationSettings.agentCount;
+        duration.value = ((int)SimulationSettings.duration / 60);
+        mapHeight.value = SimulationSettings.Height;
+        mapWidth.value = SimulationSettings.Width;
+        mapDepth.value = SimulationSettings.Depth;
+        toggleSeed.value = SimulationSettings.useRandomSeed;
     }
 
     private String AlgorithmIndexToString(int algorithmIndex){
