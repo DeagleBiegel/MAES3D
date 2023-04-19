4705ffb7787c520e3dfc5a831a1ab20a5cf1cd58
Kasperjhn <kjhn@live.dk>
2023-04-19 11:58:01 +0200

    Update SimulationSettings.cs
    It is now matching the ui defaults

--- Assets/Scripts/SimulationSettings.cs
+++ Assets/Scripts/SimulationSettings.cs
@@ -5,14 +5,14 @@ using UnityEngine;
 public static class SimulationSettings
 {
     public static int Instance = 0;
-    public static int Height = 100;
-    public static int Width = 100;
-    public static int Depth = 100;
+    public static int Height = 50;
+    public static int Width = 50;
+    public static int Depth = 50;
     public static int smoothingIterations = 20;
-	public static bool useRandomSeed = false;
+	public static bool useRandomSeed = true;
 	public static int seed = 1; 
-    public static int agentCount = 1;
-    public static int algorithm = 1; // 0 = RBW, 1 = LVD
+    public static int agentCount = 3;
+    public static int algorithm = 0; // 0 = RBW, 1 = LVD, 2 = DSVP
 
     public static float duration = 30 * 60; // in minutes
 
