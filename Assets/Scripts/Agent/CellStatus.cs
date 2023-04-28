89ecae7c56bb1c7cb505d945b23a6d552898911f
DeagleBiegel <47975952+DeagleBiegel@users.noreply.github.com>
2023-04-28 15:43:36 +0200

    Create CellStatus.cs

---
+++ Assets/Scripts/Agent/CellStatus.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MAES3D {
+    public enum CellStatus {
+            unexplored,
+            explored,
+            wall,
+            covered
+    }
+}
