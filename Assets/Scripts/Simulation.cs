013068d0770fc281c10cbdfb9ac2583e7e17fb99
Naske <thomasstubkjaer@gmail.com>
2023-05-01 14:30:48 +0200

    applied to new prefab

--- Assets/Scripts/Simulation.cs
+++ Assets/Scripts/Simulation.cs
@@ -42,7 +42,11 @@ namespace MAES3D {
 
         public void SetupScenario() {
             GameObject gameObject = Instantiate(MapPrefab, parent: transform);
-            Chunk map = (Chunk) gameObject.GetComponent(typeof(Chunk));
+            //Chunk mapc = (Chunk) gameObject.GetComponent(typeof(Chunk));
+
+            MapGenerator mapGen = new RandomConnectedSpheres();
+            Map map = (Map)gameObject.GetComponent(typeof(Map));
+            map.InitMap(mapGen.GenerateMap());
 
             GameObject cameraObject = GameObject.FindWithTag("MainCamera");
             CameraController cameraController;
